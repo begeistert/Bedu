e0c88633b885b1f851566f9dcf5ad10401bf7d97
Iván Montiel cardona <ivan_montiel2013@hotmail.com>
2020-11-24 22:51:49 -0600

    Auto stash before rebase of "origin/main"

--- Sesion-01/Reto-02/Reto 2.sql
+++ Sesion-01/Reto-02/Reto 2.sql
@@ -1,18 +1,21 @@
+USE tienda;
+
+
 SELECT nombre
 FROM empleado
 WHERE id_puesto = 4;
 
-select *
+SELECT *
 FROM puesto
 WHERE salario > 10000;
 
-select *
+SELECT *
 FROM articulo
 WHERE precio > 1000
   AND iva > 100;
 
-select *
-From venta
+SELECT *
+FROM venta
 WHERE id_articulo IN(135, 963)
   AND id_empleado IN(835, 369);
 
